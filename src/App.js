e3331c0c83d29301779184122d9bc57e74725711
KatoJun <jun.kato0208@gmail.com>
2021-10-26 12:23:53 +0900

    ページの作成完了

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import "./styles.css";
 import { Router } from "./router/Router";
 
-const user = {
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  name: "Kato Jun",
-  email: "12345@example.com",
-  phone: "090-1111-222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((value) => {
+  return {
+    id: value,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    name: `Kato Jun${value}`,
+    email: "12345@example.com",
+    phone: "090-1111-222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
