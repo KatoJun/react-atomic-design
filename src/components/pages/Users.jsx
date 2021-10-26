acf23996a91bdda97ccdcf2b7682cb651c214aec
KatoJun <jun.kato0208@gmail.com>
2021-10-26 14:49:57 +0900

    定義したグローバルstateを利用するように変更

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
-import { useLocation } from "react-router-dom";
 
 const users = [...Array(10).keys()].map((value) => {
   return {
@@ -18,17 +17,13 @@ const users = [...Array(10).keys()].map((value) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  console.log(state);
-  const isAdmin = state ? state.isAdmin : false;
-
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={user.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
