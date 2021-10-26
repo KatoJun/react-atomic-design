5ea31581ddff7768a77488f26973366adf4725d9
KatoJun <jun.kato0208@gmail.com>
2021-10-26 10:38:37 +0900

    UserCardのレイアウトまで

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UseCard";
 import "./styles.css";
 
 export default function App() {
@@ -8,9 +9,9 @@ export default function App() {
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
-
       <br />
       <SearchInput />
+      <UserCard />
     </div>
   );
 }
---
+++ src/components/organisms/user/UseCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-9999-88888</dd>
+        <dt>会社名</dt>
+        <dd>XXXX株式会社</dd>
+        <dt>WEB</dt>
+        <dd>https://dsfsdsa.com</dd>
+      </dl>
+    </div>
+  );
+};
