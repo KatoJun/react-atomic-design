d1cae0e8c37c27b2fb60bc4c3ae1f8798021d72a
KatoJun <jun.kato0208@gmail.com>
2021-10-26 11:43:06 +0900

    ヘッダのCSSあて

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   const user = {
@@ -19,13 +20,14 @@ export default function App() {
   };
 
   return (
-    <HeaderOnly>
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </HeaderOnly>
+    <BrowserRouter>
+      <HeaderOnly>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </HeaderOnly>
+    </BrowserRouter>
   );
 }
-[];
