6ee88e4baf831020c3aae8fc1d3a97424065d701
KatoJun <jun.kato0208@gmail.com>
2021-10-26 12:10:12 +0900

    ルーティングの設定

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import { useParams } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
-import { BrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
+
+const user = {
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  name: "Kato Jun",
+  email: "12345@example.com",
+  phone: "090-1111-222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
-  const user = {
-    image: "https://source.unsplash.com/NE0XGVKTmcA",
-    name: "Kato Jun",
-    email: "12345@example.com",
-    phone: "090-1111-222",
-    company: {
-      name: "テスト株式会社"
-    },
-    website: "https://google.com"
-  };
-
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
